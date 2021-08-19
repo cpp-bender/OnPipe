5b8277b7be9e33936da6876d21c94eb276738c5d
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-08-19 10:17:11 +0300

    add: Player scale logic, SingletonMonoBehavior.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] PlayerData playerData;
+
+    private void Update()
+    {
+        Scale();
+    }
+
+    private void Scale()
+    {
+        //Scales player object either down or up
+        if (Input.GetMouseButton(0))
+        {
+            ScaleDown();
+        }
+        else
+        {
+            ScaleUp();
+        }
+    }
+
+    private void ScaleDown()
+    {
+        //Scales down player object
+        float currentScaleValue = transform.localScale.x;
+        transform.localScale = new Vector3(
+            Mathf.Max(.5f, currentScaleValue - (playerData.ScaleDownFactor * Time.deltaTime)),
+            transform.localScale.y,
+            Mathf.Max(.5f, currentScaleValue - (playerData.ScaleDownFactor * Time.deltaTime)));
+    }
+
+    private void ScaleUp()
+    {
+        //Scales up player object
+        float currentScaleValue = transform.localScale.x;
+        transform.localScale = new Vector3(
+            Mathf.Min(playerData.MaxScaleValue, currentScaleValue + (playerData.ScaleUpFactor * Time.deltaTime)),
+            transform.localScale.y,
+            Mathf.Min(playerData.MaxScaleValue, currentScaleValue + (playerData.ScaleUpFactor * Time.deltaTime)));
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Player Data", fileName = "Player Data")]
+public class PlayerData : ScriptableObject
+{
+    [SerializeField] float scaleDownFactor;
+    [SerializeField] float scaleUpFactor;
+    [SerializeField] float maxScaleValue;
+
+    public float ScaleDownFactor { get => scaleDownFactor; }
+    public float ScaleUpFactor { get => scaleUpFactor; }
+    public float MaxScaleValue { get => maxScaleValue; }
+}
