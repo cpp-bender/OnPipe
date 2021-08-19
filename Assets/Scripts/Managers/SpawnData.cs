b9d6b24e0d2e5a50edf1ea84e2197354de2f5703
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-08-19 13:29:20 +0300

    add: SpawnManager.cs: Path spawn logic

---
+++ Assets/Scripts/Managers/SpawnData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Spawn Data", fileName = "Spawn Data")]
+public class SpawnData : ScriptableObject
+{
+    [Header("Pipe Prefabs")]
+    [SerializeField] GameObject largePipePrefab;
+    [SerializeField] GameObject smallPipePrefab;
+    [SerializeField] GameObject finishPipePrefab;
+
+    [Header("Pipe Prefab Values")]
+    [SerializeField] [Range(1f, 100f)] int smallPipeSpawnChance;
+    [Range(1, 7)] [SerializeField] int pathLength;
+    [Range(1f, 10f)] [SerializeField] float moveSpeed;
+
+    public GameObject LargePipePrefab { get => largePipePrefab; }
+    public GameObject SmallPipePrefab { get => smallPipePrefab; }
+    public GameObject FinishPipePrefab { get => finishPipePrefab; }
+    public int PathLength { get => pathLength; }
+    public int SmallPipeSpawnChance { get => smallPipeSpawnChance; }
+    public float MoveSpeed { get => moveSpeed; }
+}
---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private const float pipeThreshold = -2;
+
+    [SerializeField] SpawnData spawnData;
+
+    private void Start()
+    {
+        SpawnPath();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (transform.position.y <= -spawnData.PathLength * 20 + 10)
+        {
+            transform.position = Vector3.zero;
+        }
+        transform.Translate(Vector3.down * Time.deltaTime * spawnData.MoveSpeed);
+    }
+
+    private void SpawnPath()
+    {
+        Vector3 pipePos = Vector3.zero;
+        float scaleValue = 0f;
+        float lastPipeScaleValue = 0f;
+        for (int i = 0; i < spawnData.PathLength; i++)
+        {
+            GameObject newPipe = null;
+            if (Random.Range(0, 100 / spawnData.SmallPipeSpawnChance) == 0)
+            {
+                newPipe = Instantiate(spawnData.SmallPipePrefab, pipePos, Quaternion.identity, transform);
+                scaleValue = newPipe.transform.localScale.y;
+                lastPipeScaleValue = newPipe.transform.localScale.x;
+            }
+            else
+            {
+                newPipe = Instantiate(spawnData.LargePipePrefab, pipePos, Quaternion.identity, transform);
+                scaleValue = newPipe.transform.localScale.y;
+                lastPipeScaleValue = newPipe.transform.localScale.x;
+            }
+            pipePos = newPipe.transform.position + (Vector3.up * scaleValue * pipeThreshold);
+        }
+        GameObject finishPipe = Instantiate(spawnData.FinishPipePrefab, pipePos, Quaternion.identity, transform);
+        finishPipe.transform.localScale = new Vector3(lastPipeScaleValue, finishPipe.transform.localScale.y, lastPipeScaleValue);
+    }
+}
