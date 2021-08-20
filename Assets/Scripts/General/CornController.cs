30c138d20dceb7507ca65db78231fbfa2a810ea4
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-08-20 15:56:22 +0300

    add CornController.cs: Create corn logic

---
+++ Assets/Scripts/General/CornController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CornController : SingletonMonoBehaviour<CornController>
+{
+    [SerializeField] CornData cornData;
+
+    public void SpawnCorn()
+    {
+        for (int i = 0; i < cornData.SoloCornCount; i++)
+        {
+            var soloCorn = Instantiate(cornData.SoloCornPrefab, Vector3.forward, Quaternion.identity, transform);
+            soloCorn.transform.RotateAround(Vector3.zero, Vector3.up, cornData.AngleBetweenPerSoloCorn * i);
+        }
+    }
+}
---
+++ Assets/Scripts/General/CornData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Corn Data", fileName = "Corn Data")]
+public class CornData : ScriptableObject
+{
+    [SerializeField] GameObject soloCornPrefab;
+    private const int soloCornCount = 32;
+    private const float angleBetweenPerSoloCorn = 12f;
+
+    public GameObject SoloCornPrefab { get => soloCornPrefab; }
+    public int SoloCornCount { get => soloCornCount; }
+    public float AngleBetweenPerSoloCorn { get => angleBetweenPerSoloCorn; }
+}
